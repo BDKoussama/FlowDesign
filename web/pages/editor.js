6456ad11043d874a1b5eba0c69dec8fd54e5bab7
BDKoussama <bdk.oussama@gmail.com>
2023-01-21 19:37:38 +0100

    disabled the editor on mobile version

--- web/pages/editor.js
+++ web/pages/editor.js
@@ -4,8 +4,7 @@ import SideBar from '../components/Editor/SideBar';
 import Widget  from '../components/Editor/Widget';
 import Scene from '../components/Scene';
 import WidgetContextProvider from '../context/WidgetContext';
-
-
+import {isMobile} from 'react-device-detect';
 
 export default function Editor() {  
 
@@ -16,14 +15,19 @@ export default function Editor() {
           <Head>
               <title>Editor App</title>
           </Head>
-
-          <div  className='editor flex flex-col-reverse md:flex-row h-[100%] md:h-[100vh]'>
-              <WidgetContextProvider>
-                  <SideBar />
-                  <Widget toggle = {toggle} setToggle = {setToggle} />
-              </WidgetContextProvider>
-              <Scene toggle = {toggle}/> 
-          </div>     
+          {isMobile ? (
+            <div className='h-screen w-full p-4 flex justify-center items-center'>
+                <p> Oops! For better experience use desktop navigator , the mobile version is stil under construction </p>
+            </div>
+          ) : (
+            <div  className='editor flex flex-col-reverse md:flex-row h-[100%] md:h-[100vh]'>
+                <WidgetContextProvider>
+                    <SideBar />
+                    <Widget toggle = {toggle} setToggle = {setToggle} />
+                </WidgetContextProvider>
+                <Scene toggle = {toggle}/> 
+            </div>
+          )}      
     </>
   )
 }
