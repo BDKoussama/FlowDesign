fc125cbe1b1e0591d4d72d5ae10f3b4f2408c398
BDKoussama <bdk.oussama@gmail.com>
2023-01-21 22:08:46 +0100

    fixed mobile version error

--- web/pages/editor.js
+++ web/pages/editor.js
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import SideBar from '../components/Editor/SideBar';
 import Widget  from '../components/Editor/Widget';
 import Scene from '../components/Scene';
+import Link from 'next/link'
 import WidgetContextProvider from '../context/WidgetContext';
 import {isMobile} from 'react-device-detect';
 
@@ -18,7 +19,12 @@ export default function Editor() {
           </Head>
           {isMobile ? (
             <div className='h-screen w-full p-4 flex flex-col justify-center items-center'>
-                <p className='text-center mb-2'> Oops! For better experience use desktop navigator , the mobile version is stil under construction </p>     
+                <p className='text-center mb-10'> Oops! For better experience use desktop navigator , the mobile version is stil under construction </p>  
+                <Link href= "/">
+                     <a>
+                         <span className="font-semibold">Go Back</span>
+                     </a>
+                </Link>   
             </div>
           ) : (
             <div  className='editor flex flex-col-reverse md:flex-row h-[100%] md:h-[100vh]'>
